8415e1b0e801e32782bad6b400c12559792f80e1
Brad Stayte <brad.stayte@e-gineering.com>
2014-04-18 11:59:42 -0400

    Base project structure for EF code first

--- EG.Kart.Presentation.Web/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EG.Kart.Presentation.Web.MvcApplication" Language="C#" %>
--- EG.Kart.Presentation.Web/Infrastructure/NavigationRoutes/INavigationRouteFilter.cs
+++
@@ -1,9 +0,0 @@
-using System.Web.Routing;
-
-namespace EG.Kart.Presentation.Web.Infrastructure.NavigationRoutes
-{
-    public interface INavigationRouteFilter
-    {
-        bool  ShouldRemove(Route navigationRoutes);
-    }
-}
